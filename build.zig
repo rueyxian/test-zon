3a97ce5a3d2d8b5b274304bf91a4bc86c6cdf054
rueyxian <yrueyxian@gmail.com>
2023-12-19 09:04:21 +0800

    root.zig counter

--- build.zig
+++ build.zig
@@ -7,17 +7,15 @@ pub fn build(b: *std.Build) void {
     // Export module
     _ = b.addModule("test-zon", .{ .source_file = .{ .path = "src/root.zig" } });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "test-zon",
-        .root_source_file = .{ .path = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "test-zon",
+    //     .root_source_file = .{ .path = "src/root.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    b.installArtifact(lib);
+    // b.installArtifact(lib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/root.zig" },
         .target = target,
@@ -25,10 +23,6 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 }
--- src/root.zig
+++ src/root.zig
@@ -9,3 +9,57 @@ pub fn hello(name: []const u8) void {
 pub fn add(a: usize, b: usize) usize {
     return a + b;
 }
+
+pub inline fn static_counter() *Counter {
+    const Context = struct {
+        var counter: Counter = .{};
+    };
+    return &Context.counter;
+}
+
+pub const Counter = struct {
+    count: usize = 0,
+    pub fn fetch_bump(self: *Counter) usize {
+        defer self.count += 1;
+        return self.count;
+    }
+    pub fn bump(self: *Counter) void {
+        self.count += 1;
+    }
+};
+
+test "static counter" {
+    const f1 = struct {
+        fn f() usize {
+            var counter = static_counter();
+            return counter.fetch_bump();
+        }
+    }.f;
+
+    const f2 = struct {
+        fn f() usize {
+            var counter = static_counter();
+            return counter.fetch_bump();
+        }
+    }.f;
+
+    const f3 = struct {
+        fn f() usize {
+            var counter = static_counter();
+            return counter.fetch_bump();
+        }
+    }.f;
+
+    try testing.expect(f1() == 0);
+    try testing.expect(f1() == 1);
+    try testing.expect(f2() == 0);
+    try testing.expect(f1() == 2);
+    try testing.expect(f3() == 0);
+    try testing.expect(f2() == 1);
+    try testing.expect(f3() == 1);
+    try testing.expect(f2() == 2);
+    try testing.expect(f2() == 3);
+    try testing.expect(f3() == 2);
+    try testing.expect(f2() == 4);
+    try testing.expect(f1() == 3);
+}
